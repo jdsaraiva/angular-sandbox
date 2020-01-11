35b4a8583cc9e3056f29f06373f4bc2b666b4d9d
jdessainsaraiva <joaodsaraiva@gmail.com>
2020-01-11 16:48:09 +0000

    1st component

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'pm-root',
+  template: '<div>' +
+              '<h1>{{pageTitle}}</h1>' +
+              '<span>My first component</span>' +
+            '</div> '
+})
+
+export class AppComponent {
+  pageTitle = 'Acme Product Management';
+}
